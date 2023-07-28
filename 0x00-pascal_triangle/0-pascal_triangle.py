3fb73dba00470a44b89e36147cba31a0d346051f
lavendahkinyua <lavendah17@gmail.com>
2023-07-28 11:28:12 +0300

    add module documentation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""Defines a function that returns a list of lists of integers"""
 def pascal_triangle(n):
     """Returns a list of lists of integers representing the Pascal’s triangle of n"""
     if n <= 0:
