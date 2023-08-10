efc5223c595ed14aebf58ef63ad5585021f085d9
lavendahkinyua <lavendah17@gmail.com>
2023-08-10 12:44:13 +0300

    task 0

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Lockboxes module"""
+
+
+def canUnlockAll(boxes):
+    """Determine if all the boxes can be opened"""
+    if type(boxes) is not list:
+        return False
+    if len(boxes) == 0:
+        return False
+    if len(boxes) == 1:
+        return True
+    keys = [0]
+    for key in keys:
+        for box in boxes[key]:
+            if box not in keys and box < len(boxes):
+                keys.append(box)
+    if len(keys) == len(boxes):
+        return True
+    else:
+        return False
