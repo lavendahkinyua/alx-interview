99669275e6f44d92588b88b33a248820dc4f6d1c
lavendahkinyua <lavendah17@gmail.com>
2023-09-07 12:00:00 +0300

    task o

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/';
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  const characters = JSON.parse(body).characters;
+  for (let i = 0; i < characters.length; i++) {
+    request(characters[i], function (error, response, body) {
+      if (error) {
+        console.error(error);
+      }
+      console.log(JSON.parse(body).name);
+    });
+  }
+});
+
+
+
+
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,1 @@
+A JavaScript scriptbthat interacts with RESTful API(star war API) to get the list of characters from a given Star Wars movie.
