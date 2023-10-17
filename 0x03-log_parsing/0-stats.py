7af3d0d8c38dece026809ded08ff285423042100
lavendahkinyua <lavendah17@gmail.com>
2023-10-17 11:09:48 +0300

    commit task

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,84 +1,59 @@
 #!/usr/bin/python3
-"""
-Log Processing Script
----------------------
 
-This script reads log lines from standard input, processes them, and prints metrics.
-"""
-
-# Import necessary modules
 import sys
 
-# Initialize variables to store metrics
-total_size = 0
-status_code_counts = {}
-
-def process_log_lines():
+def print_metrics(status_code_counts, total_file_size):
     """
-    Read log lines from standard input, process them, and print metrics.
-
-    This function reads log lines line by line from standard input and computes
-    two types of metrics:
-    1. Total file size (sum of all file sizes encountered).
-    2. Counts of status codes (200, 301, 400, 401, 403, 404, 405, and 500).
-
-    It prints these metrics every 10 lines or when interrupted by CTRL+C.
-
-    The input format for log lines should be as follows:
-    <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
-    If a line does not match this format, it will be skipped.
-
-    The function doesn't return anything and runs indefinitely until interrupted.
+    Print the computed metrics.
 
     Args:
-        None
+        status_code_counts (dict): A dictionary containing status code counts.
+        total_file_size (int): The total file size.
 
     Returns:
         None
     """
+    print("Total file size: File size:", total_file_size)
+    for code, count in sorted(status_code_counts.items(), key=lambda x: int(x[0])):
+        if count != 0:
+            print(f"{code}: {count}")
+
+def main():
+    total_file_size = 0
+    status_code_counts = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0
+    }
+    line_counter = 0
+
     try:
-        line_number = 0
-        while True:
-            line = sys.stdin.readline()
-            if not line:
-                break  # End of input
+        for line in sys.stdin:
+            line_parts = line.split()
 
-            line = line.strip()
+            if len(line_parts) == 7:
+                file_size = int(line_parts[-1])
+                status_code = line_parts[-2]
 
-            # Parse the line to extract the relevant information
-            parts = line.split()
-            if len(parts) != 7:
-                continue  # Skip lines that don't match the expected format
+                total_file_size += file_size
 
-            ip_address, date, request, status_code, file_size = parts
+                if status_code in status_code_counts:
+                    status_code_counts[status_code] += 1
 
-            try:
-                file_size = int(file_size)
-            except ValueError:
-                continue  # Skip lines with non-integer file size
+                line_counter += 1
 
-            # Update total file size
-            total_size += file_size
-
-            # Update status code counts
-            if status_code in {"200", "301", "400", "401", "403", "404", "405", "500"}:
-                if status_code not in status_code_counts:
-                    status_code_counts[status_code] = 0
-                status_code_counts[status_code] += 1
-
-            line_number += 1
-
-            # Print statistics every 10 lines or on keyboard interruption (CTRL+C)
-            if line_number % 10 == 0:
-                print("Total file size: File size:", total_size)
-                for code in sorted(status_code_counts.keys(), key=lambda x: int(x)):
-                    print(f"{code}: {status_code_counts[code]}")
+                if line_counter == 10:
+                    print_metrics(status_code_counts, total_file_size)
+                    line_counter = 0
 
     except KeyboardInterrupt:
-        # Handle keyboard interruption (CTRL+C)
-        print("Total file size: File size:", total_size)
-        for code in sorted(status_code_counts.keys(), key=lambda x: int(x)):
-            print(f"{code}: {status_code_counts[code}")
+        print_metrics(status_code_counts, total_file_size)
 
 if __name__ == "__main__":
-    process_log_lines()
+    main()
+
