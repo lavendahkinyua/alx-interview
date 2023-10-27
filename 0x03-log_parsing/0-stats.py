7145e34c6996099327964daf98b051a25d57ac8b
lavendahkinyua <lavendah17@gmail.com>
2023-10-27 14:06:20 +0300

    log parsing file commit

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -20,64 +20,48 @@ status codes should be printed in ascending order
 
 Warning: In this sample, you will have random values - it’s normal to not have the same output as this one.
 """
-
 import sys
 
-def print_metrics(status_code_counts, total_file_size):
-    """
-    Print the computed metrics.
 
-    Args:
-        status_code_counts (dict): A dictionary containing status code counts.
-        total_file_size (int): The total file size.
+def print_msg(codes, file_size):
+    print("File size: {}".format(file_size))
+    for key, val in sorted(codes.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
 
-    Returns:
-        None
-    """
-    print("Total file size:", total_file_size)
-    for code in sorted(status_code_counts.keys(), key=lambda x: int(x)):
-        count = status_code_counts[code]
-        if count > 0:
-            print(f"{code}: {count}")
 
-def main():
-    total_file_size = 0
-    status_code_counts = {
-        "200": 0,
-        "301": 0,
-        "400": 0,
-        "401": 0,
-        "403": 0,
-        "404": 0,
-        "405": 0,
-        "500": 0
-    }
-    line_counter = 0
+file_size = 0
+code = 0
+count_lines = 0
+codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
 
-    try:
-        for line in sys.stdin:
-            line_parts = line.split()
+try:
+    for line in sys.stdin:
+        parsed_line = line.split()
+        parsed_line = parsed_line[::-1]
 
-            if len(line_parts) == 7:
-                file_size = line_parts[-1]
-                status_code = line_parts[-2]
+        if len(parsed_line) > 2:
+            count_lines += 1
 
-                if status_code in status_code_counts:
-                    status_code_counts[status_code] += 1
+            if count_lines <= 10:
+                file_size += int(parsed_line[0])
+                code = parsed_line[1]
 
-                try:
-                    total_file_size += int(file_size)
-                except ValueError:
-                    pass
+                if (code in codes.keys()):
+                    codes[code] += 1
 
-                line_counter += 1
+            if (count_lines == 10):
+                print_msg(codes, file_size)
+                count_lines = 0
 
-                if line_counter == 10:
-                    print_metrics(status_code_counts, total_file_size)
-                    line_counter = 0
-
-    except KeyboardInterrupt:
-        print_metrics(status_code_counts, total_file_size)
-
-if __name__ == "__main__":
-    main()
+finally:
+    print_msg(codes, file_size)
