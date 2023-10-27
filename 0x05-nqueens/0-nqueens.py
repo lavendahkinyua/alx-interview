5425810067e3754997fbba59d41c3ba084f2b8be
lavendahkinyua <lavendah17@gmail.com>
2023-10-27 17:36:05 +0300

    task commit

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -26,36 +26,60 @@ if __name__ == '__main__':
   c = 0
 
   while r < n:
+    goback = False
     while c < n:
-      if len(queens) == 0:
-        queens.append([r, c])
-        solving.append([r, c])
-      else:
-        for queen in queens:
-          if c == queen[1]:
-            stop = True
-            break
-          if abs(r - queen[0]) == abs(c - queen[1]):
-            stop = True
-            break
-        if not stop:
-          queens.append([r, c])
-          solving.append([r, c])
-      if stop:
-        stop = False
-      else:
-        break
-      c += 1
-    if len(queens) == r:
-      queens.pop()
-      if len(queens) == 0:
-        print('N must be at least 4')
-        exit(1)
-      r = queens[-1][0]
-      c = queens[-1][1] + 1
-      queens.pop()
-    else:
-      r += 1
-      c = 0
+        safe = True
+        for cord in placed_queens:
+            col = cord[1]
+            if(col == c or col + (r-cord[0]) == c or
+                    col - (r-cord[0]) == c):
+                safe = False
+                break
 
-    print(solving)
+        if not safe:
+            if c == n - 1:
+                goback = True
+                break
+            c += 1
+            continue
+
+           
+        cords = [r, c]
+        placed_queens.append(cords)
+        if r == n - 1:
+            solutions.append(placed_queens[:])
+            for cord in placed_queens:
+                if cord[1] < n - 1:
+                    r = cord[0]
+                    c = cord[1]
+            for i in range(n - r):
+                placed_queens.pop()
+            if r == n - 1 and c == n - 1:
+                placed_queens = []
+                stop = True
+            r -= 1
+            c += 1
+        else:
+            c = 0
+        break
+    if stop:
+        break
+       
+    if goback:
+        r -= 1
+        while r >= 0:
+            c = placed_queens[r][1] + 1
+            del placed_queens[r]
+            if c < n:
+                break
+            r -= 1
+        if r < 0:
+            break
+        continue
+    r += 1
+
+for idx, val in enumerate(solutions):
+    if idx == len(solutions) - 1:
+        print(val, end='')
+    else:
+        print(val)
